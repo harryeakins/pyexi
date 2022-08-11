0bebd789d513668c1f46dd4df3642d52a4e00e98
Harry <harry@justtraffic.io>
2022-08-11 13:08:50 -0700

    WIP (changing machines)

--- tests/test_pyexi.py
+++ tests/test_pyexi.py
@@ -1,5 +1,15 @@
-from pyexi.pyexi import ExiEncoder
+from pyexi.pyexi import ExiEncoder, ExiSchemaHandler
+from xml import sax
+import xmlschema
 
 def test_can_instantiate():
     encoder = ExiEncoder()
     assert encoder is not None
+
+def test_can_use_handler():
+    # sax.parse("tests/fixtures/notebook.xsd", ExiSchemaHandler())
+    schema = xmlschema.XMLSchema("tests/fixtures/notebook.xsd")
+    for name in schema.elements:
+        element = schema.elements[name]
+        print(element)
+        print(element.type)
